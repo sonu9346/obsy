e499914299a3243d29dcb2d5a1f0f24f268f131d
sonu9346 <rahulbangle20@gmail.com>
2023-12-19 06:25:07 +0530

    The (generated by blackbox.ai )

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var cursor = document.querySelector('#cursor');
 var main = document.querySelector('#main');
 var body = document.querySelector('body');
 var tl = gsap.timeline();
-var vidContainer = document.querySelector('#vid-container');
+var img = document.querySelector('#img');
 var playReel = document.querySelector('#play-reel');
 // Define the 'loader' function
 
@@ -41,7 +41,6 @@ function locomotive() {
   ScrollTrigger.refresh();
   console.log('3 done');
 }
-
 function loader() {
   // Define a variable called 'count' and set it to 0
   var count = 0;
@@ -98,17 +97,107 @@ function loader() {
   });
   console.log('working');
 }
-
 function cursorAnimation() {
-  main.addEventListener('mousemove', function (dets) {
-    gsap.to(cursor, {
-      x: dets.x,
+  Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: 'cubic-bezier(0.23, 1, 0.320, 1)',
+    duration: 1,
+  });
+  Shery.makeMagnet('nav h4');
+  img.addEventListener('mouseenter', function () {
+    img.addEventListener('mousemove', function (dets) {
+      gsap.to(playReel, {
+        left: dets.x - 570,
+        y: dets.y - 300,
+      });
+    });
+  });
+}
+
+function sheryAnimation() {
+  Shery.imageEffect('.image-div', {
+    style: 2,
+    // debug: true,
+    // gooey: true,
+    config: {
+      resolutionXY: { value: 100 },
+      distortion: { value: true },
+      mode: { value: -3 },
+      mousemove: { value: 0 },
+      modeA: { value: 1 },
+      modeN: { value: 0 },
+      speed: { value: 1, range: [-500, 500], rangep: [-10, 10] },
+      frequency: { value: 50, range: [-800, 800], rangep: [-50, 50] },
+      angle: { value: 0.5, range: [0, 3.141592653589793] },
+      waveFactor: { value: 1.4, range: [-3, 3] },
+      color: { value: 10212607 },
+      pixelStrength: { value: 3, range: [-20, 100], rangep: [-20, 20] },
+      quality: { value: 5, range: [0, 10] },
+      contrast: { value: 1, range: [-25, 25] },
+      brightness: { value: 1, range: [-1, 25] },
+      colorExposer: { value: 0.18, range: [-5, 5] },
+      strength: { value: 0.2, range: [-40, 40], rangep: [-5, 5] },
+      exposer: { value: 8, range: [-100, 100] },
+      zindex: { value: -9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.7272515295014734 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: true },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: false },
+      maskVal: { value: 1, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.92, range: [0, 10] },
+      metaball: { value: 0.4, range: [0, 2] },
+      discard_threshold: { value: 0.6, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.5, range: [0, 2] },
+      noise_scale: { value: 10.69, range: [0, 100] },
+    },
+  });
+}
+
+function flagAnimation() {
+  document.addEventListener('mousemove', function (dets) {
+    gsap.to('#flag', {
+      x: dets.x - 500,
       y: dets.y,
     });
   });
-  Shery.makeMagnet('nav h4');
+  document.querySelector('#hero3').addEventListener('mouseenter', function () {
+    gsap.to('#flag', {
+      opacity: 1,
+    });
+  });
+  document.querySelector('#hero3').addEventListener('mouseleave', function () {
+    gsap.to('#flag', {
+      opacity: 0,
+    });
+  });
 }
+// document.querySelector('#hero3').addEventListener('mouseleave', function () {
+//   document.querySelector('#flag').addEventListener('mousemove', function () {
+//     gsap.to('#flag', {
+//       opacity: 0,
+//     });
+//   });
+// });
 
 loader();
 cursorAnimation();
 locomotive();
+sheryAnimation();
+flagAnimation();
